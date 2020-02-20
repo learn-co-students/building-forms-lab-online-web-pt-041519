9d19067e0a8346127762aca40d2ba1fe65133566
Johnson <jhonsonj88@gmail.com>
2020-02-20 10:33:58 -0800

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,11 +1,58 @@
 // Add BandInput component
+  
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+  state = {
+    name: ''
+  }
+
+  // Change state value
+  handleOnChange = event => {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  // Create band objects
+  handleOnSubmit = event => {
+    event.preventDefault();
+
+    // Create object from state
+    const band = {...this.state};
+
+    // Pass band object to action creator
+    this.props.addBand(band)
+
+    // Return component state to default
+    this.setState({
+      name: '',
+    });
+  }
+
+  // Display band creation form with text input and submit button
   render() {
     return(
       <div>
-        Band Input
+        <form className="form-horizontal" onSubmit={this.handleOnSubmit}>
+          <div className="form-group">
+            <label htmlFor="band" className="col-md-4 control-label">Band Name</label>
+            <div className="col-md-5">
+              <input
+                className="form-control"
+                name="name"
+                type="text"
+                value={this.state.name}
+                onChange={this.handleOnChange}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="col-md-6 col-md-offset-4">
+              <button type="submit" className="btn btn-primary">Add Band</button>
+            </div>
+          </div>
+        </form>
       </div>
     )
   }
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import BandInput from '../components/BandInput'
+import Bands from '../components/Bands';
+
 
 class BandsContainer extends Component {
+
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.props.addBand} />
+        <Bands bands={this.props.bands} />
       </div>
     )
   }
 }
 
-export default BandsContainer
+// Make bands available as props
+const mapStateToProps = ({ bands }) => ({ bands })
+
+// Make addBand available as addBand
+const mapDispatchToProps = dispatch => ({ addBand: band => dispatch({ type: "ADD_BAND", band }) })
+
+// Connect BandsContainer to store
+export default connect(mapStateToProps, mapDispatchToProps )(BandsContainer)
